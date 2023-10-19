c40c4360f0a612cc09553290d2fa13908adcc4d7
Mariana Silva de Godoi <godoi.mariana@hcosta.com.br>
2023-10-19 16:01:55 -0300

    inicio estoque

---
+++ php/acao.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'classes/conexao.php';
+
+$con = new Conexao();
+
+$acao = $_GET['acao'];
+
+switch ($acao) {
+    case 'editar':
+
+    break;
+    case 'editar':
+
+    break;
+    case 'editar':
+
+    break;
+    
+    default:
+
+    break;
+}
+
+?>
