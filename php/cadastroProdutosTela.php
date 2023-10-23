91fb0bb3cad8b4945b7e44333e5cf6fc16f5639f
Witin12 <143944890+Witin12@users.noreply.github.com>
2023-10-23 04:11:50 +0000

    css do cadastro produto

--- php/cadastroProdutosTela.php
+++ php/cadastroProdutosTela.php
@@ -3,44 +3,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de produto</title>
-    <style>
-        body {
-            background-color: grey;
-        }
-    </style>
+
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../css/cadastroProduto.css">
 </head>
 <body>
     <div>
         <?php
             if(isset($_GET['tela']) && $_GET['tela'] === 'add') {
                 ?>
-                <div>Cadastro de produtos</div>
-                    <div>
-                        <form action="../php/cadastroProdutos.php" method="POST">
-                            <input type="hidden" id="situacao" name="situacao" value="TRUE">
-                            <input type="hidden" id="acao" name="acao" value="incluir">
-                            <label for="imagem">Selecione uma imagem:</label>
-                            <img src="../img/addImg.svg">
-                            <input type="file" id="imagem" name="imagem">
-                            <label>Nome</label>
-                            <input type="text" id="nome" name="nome">
-                            <label>Descrição</label>
-                            <input type="text" id="desc" name="desc">
-                            <label>Categoria</label>
-                            <input type="number" id="categoria" name="categoria">
-                            <label>Preço</label>
-                            <input type="text" id="preco" name="preco">
-                            <label>Quantidade</label>
-                            <input type="number" id="qtd" name="qtd">
-                            <div>
-                                <div>
-                                    <button type="submit" class="button" id="button">Salvar</button>
+                <div class="container">
+                    <div class="titulo">Cadastro de produtos</div>
+                        <div class="forms">
+                            <form action="../php/cadastroProdutos.php" method="POST">
+                                <input type="hidden" id="situacao" name="situacao" value="TRUE">
+                                <input type="hidden" id="acao" name="acao" value="incluir">
+                                <label for="imagem">Selecione uma imagem:</label>
+                                <img src="../img/addImg.svg">
+                                <input type="file" id="imagem" name="imagem">
+                                <label>Nome</label>
+                                <input type="text" id="nome" name="nome" class="info">
+                                <label>Descrição</label>
+                                <input type="text" id="desc" name="desc" class="info">
+                                <label>Categoria</label>
+                                <input type="number" id="categoria" name="categoria" class="info">
+                                <label>Preço</label>
+                                <input type="text" id="preco" name="preco" class="info">
+                                <label>Quantidade</label>
+                                <input type="number" id="qtd" name="qtd" class="info">
+                                <div class="botoes">
+                                    <div>
+                                        <button type="submit" class="button" id="button">Salvar</button>
+                                    </div>
+                                    <div>
+                                        <button type="submit" class="button" id="button">Editar</button>
+                                    </div>
                                 </div>
-                                <div>
-                                    <button type="submit" class="button" id="button">Editar</button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 <?php
             } else {
@@ -49,31 +49,33 @@
                 $result = pesquisar("produtos", "WHERE id =".$_GET['id']);
 
                 ?>
-                <div>Alteração de produtos</div>
-                <div>
-                    <form action="cadastroProdutos.php" method="POST">
-                        <input type="hidden" id="situacao" name="situacao" value="TRUE">
-                        <input type="hidden" id="acao" name="acao" value="alterar">
-                        <input type="hidden" id="id" name="id" value="<?php echo $result[0]['id']?>">
-                        <label for="imagem">Selecione uma imagem:</label>
-                        <img src="../img/addImg.svg">
-                        <input type="file" id="imagem" name="imagem"  value="<?php echo $result[0]['imagem']?>">
-                        <label>Nome</label>
-                        <input type="text" id="nome" name="nome" value="<?php echo $result[0]['nome']?>">
-                        <label>Descrição</label>
-                        <input type="text" id="desc" name="desc"  value="<?php echo $result[0]['descricao']?>">
-                        <label>Categoria</label>
-                        <input type="number" id="categoria" name="categoria"  value="<?php echo $result[0]['categoria']?>">
-                        <label>Preço</label>
-                        <input type="text" id="preco" name="preco"  value="<?php echo $result[0]['preco']?>">
-                        <label>Quantidade</label>
-                        <input type="number" id="qtd" name="qtd"  value="<?php echo $result[0]['qntd']?>">
-                        <div>
-                            <div>
-                                <button type="submit" class="button" id="button">Salvar</button>
+                <div class="container">
+                    <div class="titulo">Alteração de produtos</div>
+                    <div class="forms">
+                        <form action="cadastroProdutos.php" method="POST">
+                            <input type="hidden" id="situacao" name="situacao" value="TRUE">
+                            <input type="hidden" id="acao" name="acao" value="alterar">
+                            <input type="hidden" id="id" name="id" value="<?php echo $result[0]['id']?>">
+                            <label for="imagem">Selecione uma imagem:</label>
+                            <img src="../img/addImg.svg">
+                            <input type="file" id="imagem" name="imagem"  value="<?php echo $result[0]['imagem']?>">
+                            <label>Nome</label>
+                            <input type="text" id="nome" class="info" name="nome" value="<?php echo $result[0]['nome']?>">
+                            <label>Descrição</label>
+                            <input type="text" id="desc" class="info" name="desc"  value="<?php echo $result[0]['descricao']?>">
+                            <label>Categoria</label>
+                            <input type="number" id="categoria" class="info" name="categoria"  value="<?php echo $result[0]['categoria']?>">
+                            <label>Preço</label>
+                            <input type="text" id="preco" class="info" name="preco"  value="<?php echo $result[0]['preco']?>">
+                            <label>Quantidade</label>
+                            <input type="number" id="qtd" class="info" name="qtd"  value="<?php echo $result[0]['qntd']?>">
+                            <div class="botoes">
+                                <div>
+                                    <button type="submit" class="button" id="button">Salvar</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             <?php
             }
